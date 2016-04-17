9a08f2b489af26dba8041956829c06d5b4b97dd0
victorlaoh <victorlaoh.mail@gmail.com>
2016-04-17 21:32:56 +0700

    Added files via upload
    first mockup screen

---
+++ js/controllers.js
@@ -0,0 +1,18 @@
+angular.module('app.controllers', [])
+  
+.controller('welcomeCtrl', function($scope) {
+
+})
+   
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('mainCtrl', function($scope) {
+
+})
+ 
---
+++ js/routes.js
@@ -0,0 +1,41 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('welcome', {
+    url: '/welcome',
+    templateUrl: 'templates/welcome.html',
+    controller: 'welcomeCtrl'
+  })
+
+  .state('signup', {
+    url: '/signup',
+    templateUrl: 'templates/signup.html',
+    controller: 'signupCtrl'
+  })
+
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('main', {
+    url: '/main',
+    templateUrl: 'templates/main.html',
+    controller: 'mainCtrl'
+  })
+
+$urlRouterProvider.otherwise('/welcome')
+
+  
+
+});
